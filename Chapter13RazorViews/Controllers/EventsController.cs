28d7e172485e00f3faef98d9c5d113ac3dd5c76e
Daryn DeMoss <darynbrennondemoss@gmail.com>
2020-06-23 14:27:57 -0400

    Add project files.

---
+++ Chapter13RazorViews/Controllers/EventsController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Chapter13RazorViews.Controllers
+{
+    public class EventsController : Controller
+    {
+        static private Dictionary<string, string> Events = new Dictionary<string, string>();
+
+        [HttpGet]
+        public IActionResult Index()
+        {                 
+
+            ViewBag.events = Events;
+
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Route("/Events/Add")]
+        public ActionResult NewEvent(string name, string desc)
+        {
+            Events.Add(name, desc);
+            
+            return Redirect("/Events");
+        }   
+
+
+    }
+}
---
+++ Chapter13RazorViews/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Chapter13RazorViews.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
