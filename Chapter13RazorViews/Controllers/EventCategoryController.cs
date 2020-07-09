e87cd151f67fcf2441893771de28de74458cbd3a
Daryn DeMoss <darynbrennondemoss@gmail.com>
2020-07-09 12:05:26 -0500

    Commits to the studio ORM

--- Chapter13RazorViews/Controllers/EventCategoryController.cs
+++ Chapter13RazorViews/Controllers/EventCategoryController.cs
@@ -1,5 +1,6 @@
 ﻿using Chapter13RazorViews.Data;
 using Chapter13RazorViews.Models;
+using Chapter13RazorViews.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
 using System.Linq;
@@ -19,6 +20,33 @@ namespace Chapter13RazorViews.Controllers
         {
             List<EventCategory> categories = _context.Categories.ToList();
             return View(categories);
+        }
+
+        public IActionResult Create()
+        {
+            AddEventCategoryViewModel viewModel = new AddEventCategoryViewModel();
+
+            return View(viewModel);
+        }
+
+        [HttpPost]
+        public IActionResult Create(AddEventCategoryViewModel viewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                EventCategory newEventCategory = new EventCategory
+                {
+                    Name = viewModel.Name
+                    
+                };
+
+                _context.Categories.Add(newEventCategory);
+                _context.SaveChanges();
+
+                return Redirect("/EventCategory");
+            }
+
+            return View(viewModel);
         }
     }
 }
---
+++ Chapter13RazorViews/ViewModels/AddEventCategoryViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Chapter13RazorViews.ViewModels
+{
+    public class AddEventCategoryViewModel
+    {
+        [Required(ErrorMessage = "Category Name is required.")]
+        [StringLength(50, MinimumLength = 3, ErrorMessage = "Category Name must be between 3 and 50 characters.")]
+        public string Name { get; set; }
+
+
+    }
+}
